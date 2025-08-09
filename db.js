f5ef8f561373d5445fe517cee4c86954cd1e1699
aaryanwadhawan7 <aaryanwadhawam339@gmail.com>
2025-08-10 03:08:26 +0530

    Added .env file to repo

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Siddhant@10',
-  database: 'school_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
-
